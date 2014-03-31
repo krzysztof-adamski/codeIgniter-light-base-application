e96065c050240ead950aa3282d40c6ec8184f627
Denzil Doyle <doyle.d@hotmail.com>
2014-03-31 08:45:04 -0400

    add files to github

---
+++ application/views/welcome_message.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/style.css">
+	<h1>Hello World!</h1>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="<?= base_url() . "public/" ?>js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
