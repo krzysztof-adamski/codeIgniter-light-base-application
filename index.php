fae1aaabbbbf0958f54aa30f72ae41e4cb4bb906
denzildoyle <yell@denzildoyle.me>
2015-01-28 22:10:14 -0400

    Refactor code

--- index.php
+++ index.php
@@ -28,10 +28,8 @@
  * By default development will show errors but testing and live will hide them.
  */
 
-if (defined('ENVIRONMENT'))
-{
-	switch (ENVIRONMENT)
-	{
+if (defined('ENVIRONMENT')){
+	switch (ENVIRONMENT){
 		case 'development':
 			error_reporting(E_ALL);
 		break;
