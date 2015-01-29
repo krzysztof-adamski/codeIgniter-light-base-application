7faf5d4882be4475847c7e8d229d76bc02bf5ace
denzildoyle <yell@denzildoyle.me>
2015-01-28 22:07:13 -0400

    Refactor code

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/style.css">
-	<h1>Hello World!</h1>
-	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="<?= base_url() . "public/" ?>js/bootstrap.min.js"></script>
-</head>
-<body>
-	
-</body>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<title>CodeIgniter Light</title>
+			<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/bootstrap.min.css">
+			<link rel="stylesheet" href="<?= base_url() . "public/" ?>css/style.css">
+		</head>
+	<body>
+		<div class="container">
+			<h1>Hello World!</h1>
+			<p>Light CodeIgniter base application is a very light weight CodeIgniter framework instance with twitter bootstrap, Redbean ORM and only mysql database drive install .htaccess files already added and config setting just how I like it.</p>
+		</div>
+		
+
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="<?= base_url() . "public/" ?>js/bootstrap.min.js"></script>
+	</body>
 </html>
